d8b36dfc2be0890e7411c13ba4e4da69386beb11
Ari Lacenski <alacenski@gmail.com>
2017-10-04 06:45:16 -0700

    Added a data source for Battle information.

---
+++ app/src/main/kotlin/au/com/tilbrook/android/rxkotlin/rxtalk/BattleProvider.kt
@@ -0,0 +1,40 @@
+package au.com.tilbrook.android.rxkotlin.rxtalk
+
+import au.com.tilbrook.android.rxkotlin.rxtalk.combinelatest.Combatant
+import rx.Observable
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class BattleProvider {
+    companion object {
+        const val INTERVAL: Int = 1;
+    }
+
+    private val houseRandom = Random(Combatant.House.values().size.toLong())
+    private val debtRandom = Random(1000000)
+    private val armySizeRandom = Random(1000000)
+    private val dragonsRandom = Random(4)
+
+    inner class Battle(val combatants: Set<Combatant>)
+
+    fun getBattles(): Observable<Battle> {
+        return Observable
+                .interval(INTERVAL.toLong(), TimeUnit.SECONDS)
+                .timeInterval()
+                .flatMap { x -> Observable.just(Battle(getRandomCombatants())) }
+    }
+
+    private fun getRandomCombatants(): Set<Combatant> {
+        val firstIndex = houseRandom.nextInt()
+        var secondIndex: Int
+        do {
+            secondIndex = houseRandom.nextInt()
+        } while (secondIndex == firstIndex)
+        return listOf(firstIndex, secondIndex).map { enumIndex ->
+            Combatant(Combatant.House.values().get(enumIndex),
+                    debtRandom.nextDouble(), armySizeRandom.nextInt(), dragonsRandom.nextInt())
+        }.toSet()
+    }
+
+
+}
